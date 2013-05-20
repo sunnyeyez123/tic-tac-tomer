887861fcf35de8f6eb124549c6480eadc5a886da
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2013-05-20 18:18:52 -0400

    renamed gamepanel to gameboard

--- src/TicTacTomer.java
+++ src/TicTacTomer.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class TicTacTomer {
 
@@ -6,6 +7,14 @@ public class TicTacTomer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// start ugly hack to get button colors on Macs.  Approved by Andrew Ash
+		try {
+		    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		// end ugly hack
 		// TODO Auto-generated method stub
 		JFrame frame = new JFrame("Tic-Tac-Tomer!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
